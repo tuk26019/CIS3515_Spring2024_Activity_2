ac78b4cacaf2004c7b6d8c1183879c06e61737b5
maklisaprifti <tuk26019@temple.edu>
2024-02-06 12:13:43 -0500

    Fixed previous problem and did step 3 to create spinner.onItemSelectedListener

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -15,22 +15,27 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 1: Populate this array */
         //val numberArray = Array Of Numbers
-        val numberArray = 1..100
-
-        for (number in numberArray) {
-            if (number % 2 == 0) {
-                println(number)
-            }
-        }
+        val numberArray = (2..100 step 2).toList().toTypedArray()
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
-        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,numberArray)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
         spinner.adapter = adapter
 
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = numberArray[position]
+                displayTextView.textSize = selectedItem.toFloat()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle case when nothing is selected (if needed)
+            }
+
+        }
+        }
 
     }
-}

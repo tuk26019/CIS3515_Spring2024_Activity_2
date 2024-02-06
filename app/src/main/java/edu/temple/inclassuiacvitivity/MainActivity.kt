df712dead39847ec284b6dac50fc4440c1e751f1
maklisaprifti <tuk26019@temple.edu>
2024-02-06 11:54:57 -0500

    create arrayAdapter for step 2

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,numberArray)
+        spinner.adapter = adapter
 
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
